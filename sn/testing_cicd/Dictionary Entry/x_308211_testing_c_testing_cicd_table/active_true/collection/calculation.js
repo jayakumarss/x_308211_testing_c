7d0329495c858af10cbd0b9eb5598de8d843be46
jayakumarss <jayakumar.sengodan@gmail.com>
2020-01-29 21:39:12 -0500

    'master' branch updated from  https://dev96952.service-now.com no-cicd

---
+++ sn/testing_cicd/Dictionary Entry/x_308211_testing_c_testing_cicd_table/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : testing_cicd
+ * ClassName   : sys_dictionary
+ * Created On  : 2020-01-30 02:32:26
+ * Created By  : admin
+ * Updated On  : 2020-01-30 02:32:26
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=37ca59d32f660010862ea55df699b6d4
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
